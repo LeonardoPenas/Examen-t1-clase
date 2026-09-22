d7c429ff6e13cb1f4064f09bc80b3c29aabb3e75
LeoPenas <n00460162@upn.pe>
2026-09-21 21:25:49 -0500

    AGREGAR ULTIMA PARTE

--- src/trabajo/t1/AlumnoController.java
+++ src/trabajo/t1/AlumnoController.java
@@ -17,5 +17,20 @@ public class AlumnoController
 {
    ArrayList<Alumno> listaAlumnos = new ArrayList();
    
+   public void agregarAlumno(Alumno alumno)
+   
+   {
+   listaAlumnos.add(alumno);
+   }
+  public void listarAlumno()
+  {
+        System.out.println("LA LISTA DE ALUMNOS: ");
+        for(int i=0; i<listaAlumnos.size();i++){
+            listaAlumnos.get(i).verDatos();
+        }
+    }
+    public void agregar(Alumno nuevoAlumno){
+        listaAlumnos.add(nuevoAlumno);
+    }
    
 }
