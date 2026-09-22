161a553423f48c09c7f49154d457d0d1891c969b
LeoPenas <n00460162@upn.pe>
2026-09-21 21:32:29 -0500

    AGREGAR ULTIMA PARTE 2

--- src/trabajo/t1/TrabajoT1.java
+++ src/trabajo/t1/TrabajoT1.java
@@ -4,6 +4,8 @@
  */
 package trabajo.t1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author UCA40428
@@ -13,8 +15,101 @@ public class TrabajoT1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in);
+
+
+        AlumnoController controlador = new AlumnoController();
+
+
+        String respuesta="s";
+
+
+        while(respuesta.equalsIgnoreCase("s")){
+
+
+            Alumno alumno = new Alumno();
+
+
+
+            System.out.println("\n***** REGISTRO DE ALUMNO *****");
+
+
+            System.out.print("Nombre: ");
+            alumno.setNombre(sc.nextLine());
+
+
+            System.out.print("Apellido: ");
+            alumno.setApellido(sc.nextLine());
+
+
+            System.out.print("Tipo documento: ");
+            alumno.setTipoDocumento(sc.nextLine());
+
+
+            System.out.print("Número documento: ");
+            alumno.setnmrodocuemnto(sc.nextLine());
+
+
+            System.out.print("Nivel socioeconómico (A/B/C): ");
+            alumno.setnivelSocioeconomico(sc.nextLine());
+
+
+            System.out.print("Tipo de beca (Ninguna/Parcial/Total): ");
+            alumno.setTipoBeca(sc.nextLine());
+
+
+            System.out.print("Pensión base: ");
+            alumno.setPensionBase(Double.parseDouble(sc.nextLine()));
+
+
+
+        
+
+            if(alumno.validarDNI() &&
+               alumno.validarNivel() &&
+               alumno.validarBeca()){
+
+
+                controlador.agregarAlumno(alumno);
+
+                System.out.println("\nAlumno registrado correctamente");
+
+
+            }
+            else{
+
+                System.out.println("\nERROR: Datos incorrectos");
+
+            }
+
+
+
+            System.out.print("\n¿Desea registrar otro alumno? s/n: ");
+
+            respuesta=sc.nextLine();
+
+
+        }
+
+
+
+        controlador.listarAlumnos();
+
+
+
     }
+
+
+
+
+    }
+
+
+
+
+    }   
+    
     
 }
