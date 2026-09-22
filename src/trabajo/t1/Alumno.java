68f7ed146202ca385ed18c7800036222a2ce6b36
LeoPenas <n00460162@upn.pe>
2026-09-21 19:46:45 -0500

    Primer commit_Examen t1

---
+++ src/trabajo/t1/Alumno.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajo.t1;
+
+/**
+ *
+ * @author UCA40428
+ */
+public class Alumno 
+{
+    
+}
